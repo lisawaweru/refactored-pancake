74ce4d00778c6d078baa3e9dab3d3f9e0582fcbc
llisawaweru <lisaannewaweru@gmail.com>
2022-02-08 13:33:37 -0600

    version one

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-import input from 'input';
+function recursion(i) {
 
-function square(size) {
-    for (let i = 1;i <= size; i += 1) {
-let output = 'i'.repeat('size')
-console.log('1');
-}
+
+console.log (i);
+recursion(i +=1);
 }
 
 async function main() {
-    const size = await input.text('Enter a size');
-    square(size);
+    recursion(0);
 }
+
 main();
